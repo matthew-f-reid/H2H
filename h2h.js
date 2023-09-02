15ee6fe08f16a94197c4985abe003eff06e66bd0
matthew-f-reid <matthew.reid@griffithuni.edu.au>
2023-09-02 23:55:46 +1000

    Revert "Revert "Merge pull request #1 from matthew-f-reid/testing""
    This reverts commit 7363a7057368b4c6124951ce7e1298ea3a61c875.

--- h2h.js
+++ h2h.js
@@ -16,22 +16,46 @@ function setup() {
     let myCanvas = createCanvas(windowWidth, windowHeight);
     myCanvas.parent('game-container');
 }
+let numObjects = 50;
 function draw() {
+    frameRate(10);
     background(220);
     if (!focused) {
+        //Draw cirlces
+        for (let i = 0; i < numObjects; i++){
+            strokeWeight(5);
+            fill(random(254),random(254),random(254));
+            ellipse(random(windowWidth-1),random(windowHeight-1),random(500));
+        }
         pause();
+    }else {
+        //Draw rectangles
+        for (let i = 0; i < numObjects; i++){
+            strokeWeight(5);
+            fill(random(254),random(254),random(254));
+            rect(random(windowWidth-1),random(windowHeight-1),random(500),random(500));
+        }
     }
 }
 function pause(){
+    let str = 24;
     stroke(255,0,0);
-    strokeWeight(6);
+    strokeWeight(str);
     //Red Cross strokes
     line(0,0,windowWidth,windowHeight);
     line(0,windowHeight,windowWidth,0);
+    //Black Cross strokes
+    stroke(0);
+    strokeWeight(str);
+    line(0,0-str,windowWidth,windowHeight-str);
+    line(0,0+str,windowWidth,windowHeight+str);
+    line(0,windowHeight-str,windowWidth,0-str);
+    line(0,windowHeight+str,windowWidth,0+str);
     //Paused text
-    strokeWeight(1);
+    strokeWeight(str);
     textSize(144);
     stroke(0,0,0);
+    fill(255,0,0);
     textAlign(CENTER);
     text("Paused",windowWidth/2,windowHeight/2);
 }
